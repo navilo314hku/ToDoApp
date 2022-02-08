131b90aedec2d18c4f7911b3542fb6a919451aa1
user.name <navilo314@gmail.com>
2022-02-08 19:33:34 +0000

    receive register.html form data by flask in lab3.py

--- lab3.py
+++ lab3.py
@@ -8,7 +8,20 @@ app = Flask(__name__)
 def index():
     return render_template('layout.html')
 
-@app.route('/register')
+@app.route('/register',methods=["POST","GET"])
 def register():
-    return render_template('register.html')
+    if request.method=="POST":
+        #submitting the form
+        usernameFromForm=request.form["username"]
+        passwordFromForm=request.form["password"]
+
+    else: #GET
+        #show the webpage
+        return render_template('register.html')
+@app.route('/db')
+def debugPage():
+    return f"username{usernameFromForm}"
+
+
+
 
