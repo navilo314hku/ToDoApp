6790114cecfb6498d6b877ce602db4a16c54c773
navilo0314 <navilo314@gmail.com>
2022-02-10 09:26:52 +0000

    handle form and display the received data to front end

--- lab3.py
+++ lab3.py
@@ -1,27 +1,30 @@
-from flask import Flask,redirect, render_template,request,session,flash
+from flask import Flask,redirect,url_for,render_template,request,session,flash
 from datetime import timedelta
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
-
+registered_username=""
+registered_password=""
 #route to the index
-@app.route('/')
-def index():
-    return render_template('layout.html')
+# @app.route('/')
+# def index():
+#
+#     return render_template('index.html')
 
 @app.route('/register',methods=["POST","GET"])
 def register():
+    global registered_username,registered_password
     if request.method=="POST":
         #submitting the form
-        usernameFromForm=request.form["username"]
-        passwordFromForm=request.form["password"]
+        registered_username=request.form["username"]
+        registered_password=request.form["password"]
+        return redirect(url_for("register"))
 
     else: #GET
         #show the webpage
-        return render_template('register.html')
-@app.route('/db')
-def debugPage():
-    return f"username{usernameFromForm}"
-#lmao 
-
-
-
+        return render_template('register.html',username=registered_username,password=registered_password)
+@app.route("/<usr>")
+def user(usr):
+    return f"<h1>{usr}</h1>"
+#lmao
+if __name__=="__main__":
+    app.run(debug=True)
